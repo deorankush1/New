8bad072e6388972feaf6deea5d9f3fca0ea3e96b
deorankush1 <ankushdeora11@gmail.com>
2018-07-15 18:00:08 +0530

    remove copy folder

--- cpindiasaree/app/code/local/Shipping/Checkout/Block/Onepage/Abstract.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'Mage/Checkout/Block/Onepage/Abstract.php';
-  
-class Shipping_Checkout_Block_Onepage_Abstract extends Mage_Checkout_Block_Onepage_Abstract{
- 		protected function _getStepCodes()
-    	{
-      	  return array('login', 'billing', 'payment', 'review');
-    	}
-	}
